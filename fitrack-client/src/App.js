47ee45df212b182910685f799d0390ebaf3fefd4
abdul farhan <kukluu@hotmail.com>
2021-04-01 00:36:04 -0700

    refactoring code

--- fitrack-client/src/App.js
+++ fitrack-client/src/App.js
@@ -14,65 +14,61 @@ import CreateWorkout from "./Components/CreateWorkout/CreateWorkout";
 import CreateUser from "./Components/CreateUser/CreateUser";
 import EditWorkout from "./Components/EditWorkout/EditWorkout";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navigation />
-          <AnimatePresence>
-            <Switch>
-              <Route
-                exact
-                path="/"
-                component={() => {
-                  return <Landing />;
-                }}
-              />
-              <Route
-                path="/workoutlist"
-                render={(props) => {
-                  return <WorkoutList {...props} />;
-                }}
-              />
-              ;
-              <Route
-                path="/createworkout"
-                render={(props) => {
-                  return <CreateWorkout {...props} />;
-                }}
-              />
-              ;
-              <Route
-                path="/createuser"
-                render={(props) => {
-                  return <CreateUser {...props} />;
-                }}
-              />
-              ;
-              <Route
-                path="/edit/:id"
-                render={(props) => {
-                  return (
-                    <EditWorkout updateUser={this.updateUser} {...props} />
-                  );
-                }}
-              />
-              ;
-              <Route
-                path="/404"
-                component={() => {
-                  return <PageNotFound />;
-                }}
-              />
-              ;
-              <Redirect to="/404" />
-            </Switch>
-          </AnimatePresence>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Navigation />
+        <AnimatePresence>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={() => {
+                return <Landing />;
+              }}
+            />
+            <Route
+              path="/workoutlist"
+              render={(props) => {
+                return <WorkoutList {...props} />;
+              }}
+            />
+            ;
+            <Route
+              path="/createworkout"
+              render={(props) => {
+                return <CreateWorkout {...props} />;
+              }}
+            />
+            ;
+            <Route
+              path="/createuser"
+              render={(props) => {
+                return <CreateUser {...props} />;
+              }}
+            />
+            ;
+            <Route
+              path="/edit/:id"
+              render={(props) => {
+                return <EditWorkout {...props} />;
+              }}
+            />
+            ;
+            <Route
+              path="/404"
+              component={() => {
+                return <PageNotFound />;
+              }}
+            />
+            ;
+            <Redirect to="/404" />
+          </Switch>
+        </AnimatePresence>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
