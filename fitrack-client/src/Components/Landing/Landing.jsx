8d5a41b560cf33a7b5e26a7859f705c3b1817d91
abdul farhan <kukluu@hotmail.com>
2021-04-03 12:28:38 -0700

    refactored scss files

--- fitrack-client/src/Components/Landing/Landing.jsx
+++ fitrack-client/src/Components/Landing/Landing.jsx
@@ -4,26 +4,50 @@ import { motion } from "framer-motion";
 import "./landing.scss";
 
 export default function Hero() {
-  const transition = {
-    duration: 1,
+  const descriptionVariants = {
+    in: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+    },
+    out: {
+      opacity: 0,
+      y: "-100vh",
+      scale: 0,
+    },
+  };
+
+  const descriptionTransition = {
+    duration: 2,
     ease: [0.43, 0.13, 0.23, 0.96],
   };
 
+  const buttonVariants = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: "-500vw",
+    },
+  };
+  const buttonTransition = {
+    type: "spring",
+    stiffness: 50,
+    duration: 1,
+  };
   return (
-    <motion.div
-      className="landing-container"
-      // initial={{ opacity: 0 }}
-      // animate={{ opacity: 1 }}
-      // exit={{ opacity: 0 }}
-    >
+    <div className="landing-container">
       <div className="landing-container__title-container">
         <h1 className="landing-container__title">FiTrack</h1>
         <motion.p
           className="landing-container__slogan"
-          initial={{ opacity: 0, y: "-100vw" }}
-          animate={{ opacity: 1, y: "0" }}
-          exit={{ opacity: 0, y: "-100vw" }}
-          transition={transition}
+          initial="out"
+          animate="in"
+          exit="out"
+          variants={descriptionVariants}
+          transition={descriptionTransition}
         >
           Helping you achieve{" "}
           <span className="landing-container__slogan-span">fitness goals</span>{" "}
@@ -33,10 +57,11 @@ export default function Hero() {
           <Link to="/createuser" className="landing-container__button-link">
             <motion.button
               className="landing-container__button"
-              initial={{ opacity: 0, x: "-500vw" }}
-              animate={{ opacity: 1, x: "0" }}
-              exit={{ opacity: 0, x: "-500vw" }}
-              transition={{ type: "spring", stiffness: 50, duration: 1 }}
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={buttonVariants}
+              transition={buttonTransition}
               whileHover={{
                 textShadow: "0 0 8px rgb(255,255,255)",
               }}
@@ -46,6 +71,6 @@ export default function Hero() {
           </Link>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 }
