e6f8a1661821b78486ca997bd13ececa37af9cde
Abdul <kukluu@hotmail.com>
2021-07-15 16:04:32 -0700

    refactor, class component to functional component create user

--- fitrack-client/src/Components/CreateUser/CreateUser.jsx
+++ fitrack-client/src/Components/CreateUser/CreateUser.jsx
@@ -1,29 +1,129 @@
-import React from "react";
+//class component kept for reference purposes
+
+// import React from "react";
+// import { motion } from "framer-motion";
+// import axios from "axios";
+// import "./createUser.scss";
+
+// export default class CreateUser extends React.Component {
+//   state = {
+//     username: "",
+//   };
+
+//   onChangeUsername = (e) => {
+//     this.setState({
+//       username: e.target.value,
+//     });
+//   };
+
+//   onSubmit = (e) => {
+//     e.preventDefault();
+//     const user = {
+//       username: this.state.username,
+//     };
+
+//     axios
+//       .post("/users/add", user)
+//       .then((res) => {
+//         this.props.history.push({
+//           pathname: "/createworkout",
+//           state: user,
+//         });
+//         console.log(res.data);
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   render() {
+//     // Framer motion animations
+//     const cardVariants = {
+//       initial: {
+//         opacity: 0,
+//         x: "-500vw",
+//         scale: 0,
+//       },
+//       in: {
+//         opacity: 1,
+//         x: 0,
+//         scale: 1,
+//       },
+//       out: {
+//         opacity: 0,
+//         x: "500vw",
+//         scale: 0,
+//       },
+//     };
+//     // Framer motion animations
+//     const cardTransition = {
+//       type: "tween",
+//       ease: "anticipate",
+//       duration: 0.8,
+//     };
+//     return (
+//       <div className="user-container">
+//         <motion.div
+//           className="user-container__card"
+//           initial="initial"
+//           animate="in"
+//           exit="out"
+//           variants={cardVariants}
+//           transition={cardTransition}
+//         >
+//           <div className="user-container__title-container">
+//             <h2 className="user-container__title">CREATE USER </h2>
+//           </div>
+//           <div className="user-container__form-container">
+//             <div className="user-container__image"></div>
+//             <form className="user-container__form" onSubmit={this.onSubmit}>
+//               <div className="user-container__username-container">
+//                 <label className="user-container__username-label" htmlFor="">
+//                   USERNAME
+//                 </label>
+//                 <input
+//                   className="user-container__user"
+//                   type="text"
+//                   value={this.state.username}
+//                   onChange={this.onChangeUsername}
+//                 />
+//               </div>
+//               <div className="user-container__button-container">
+//                 <button className="user-container__button">CREATE</button>
+//               </div>
+//             </form>
+//           </div>
+//         </motion.div>
+//       </div>
+//     );
+//   }
+// }
+
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom";
 import { motion } from "framer-motion";
 import axios from "axios";
 import "./createUser.scss";
 
-export default class CreateUser extends React.Component {
-  state = {
-    username: "",
-  };
+ const CreateUser = () => {
+  
+  const [userName, setUserName] = useState('')
+  let history = useHistory()
 
-  onChangeUsername = (e) => {
-    this.setState({
-      username: e.target.value,
-    });
-  };
 
-  onSubmit = (e) => {
+  function onChangeUsername (e){
+    setUserName(e.target.value)
+  };
+  
+  
+  function handleSubmit(e) {
     e.preventDefault();
     const user = {
-      username: this.state.username,
+      username: userName,
     };
-
+  
     axios
       .post("/users/add", user)
       .then((res) => {
-        this.props.history.push({
+        history.push({
           pathname: "/createworkout",
           state: user,
         });
@@ -32,65 +132,72 @@ export default class CreateUser extends React.Component {
       .catch((err) => console.log(err));
   };
 
-  render() {
-    // Framer motion animations
-    const cardVariants = {
-      initial: {
-        opacity: 0,
-        x: "-500vw",
-        scale: 0,
-      },
-      in: {
-        opacity: 1,
-        x: 0,
-        scale: 1,
-      },
-      out: {
-        opacity: 0,
-        x: "500vw",
-        scale: 0,
-      },
-    };
-    // Framer motion animations
-    const cardTransition = {
-      type: "tween",
-      ease: "anticipate",
-      duration: 0.8,
-    };
-    return (
-      <div className="user-container">
-        <motion.div
-          className="user-container__card"
-          initial="initial"
-          animate="in"
-          exit="out"
-          variants={cardVariants}
-          transition={cardTransition}
-        >
-          <div className="user-container__title-container">
-            <h2 className="user-container__title">CREATE USER </h2>
-          </div>
-          <div className="user-container__form-container">
-            <div className="user-container__image"></div>
-            <form className="user-container__form" onSubmit={this.onSubmit}>
-              <div className="user-container__username-container">
-                <label className="user-container__username-label" htmlFor="">
-                  USERNAME
-                </label>
-                <input
-                  className="user-container__user"
-                  type="text"
-                  value={this.state.username}
-                  onChange={this.onChangeUsername}
-                />
-              </div>
-              <div className="user-container__button-container">
-                <button className="user-container__button">CREATE</button>
-              </div>
-            </form>
-          </div>
-        </motion.div>
+  // Framer motion animations
+  const cardVariants = {
+    initial: {
+      opacity: 0,
+      x: "-500vw",
+      scale: 0,
+    },
+    in: {
+      opacity: 1,
+      x: 0,
+      scale: 1,
+    },
+    out: {
+      opacity: 0,
+      x: "500vw",
+      scale: 0,
+    },
+  };
+  // Framer motion animations
+  const cardTransition = {
+    type: "tween",
+    ease: "anticipate",
+    duration: 0.8,
+  };
+
+  return (
+    <div className="user-container">
+    <motion.div
+      className="user-container__card"
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={cardVariants}
+      transition={cardTransition}
+    >
+      <div className="user-container__title-container">
+        <h2 className="user-container__title">CREATE USER </h2>
       </div>
-    );
-  }
+      <div className="user-container__form-container">
+        <div className="user-container__image"></div>
+        <form className="user-container__form" 
+        
+        onSubmit={handleSubmit}
+        
+        >
+          <div className="user-container__username-container">
+            <label className="user-container__username-label" htmlFor="">
+              USERNAME
+            </label>
+            <input
+              className="user-container__user"
+              type="text"
+              value={userName}
+              onChange={onChangeUsername}
+            />
+          </div>
+          <div className="user-container__button-container">
+            <button className="user-container__button">CREATE</button>
+          </div>
+        </form>
+      </div>
+    </motion.div>
+  </div>
+  )
 }
+
+export default CreateUser
+
+
--- fitrack-client/src/Components/CreateWorkout/CreateWorkout.jsx
+++ fitrack-client/src/Components/CreateWorkout/CreateWorkout.jsx
@@ -61,12 +61,16 @@ export default class CreateWorkout extends React.Component {
       date: this.state.date,
     };
 
+    const name = {
+      username: this.state.username,
+    };
+
     axios
       .post("/workouts/add", workout)
       .then((res) => {
         this.props.history.push({
           pathname: "/workoutlist",
-          state: { username: this.state.username },
+          state: { username: name },
         });
         console.log(res.data);
       })
@@ -183,3 +187,221 @@ export default class CreateWorkout extends React.Component {
     );
   }
 }
+
+// import React, { useState, useEffect } from "react";
+// import { motion } from "framer-motion";
+// import axios from "axios";
+// import "./createWorkout.scss";
+// import {
+//   useHistory,
+
+//   // useLocation
+// } from "react-router-dom";
+
+// const CreateWorkout = () => {
+//   const [userName, setUserName] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [duration, setDuration] = useState(0);
+//   const [date, setDate] = useState(new Date());
+//   const [users, setUsers] = useState([]);
+
+//   const history = useHistory();
+//   // const location = useLocation();
+
+//   useEffect(() => {
+//     axios
+//       .get("/users")
+//       .then((res) => {
+//         // setUserName(res.data[7].username);
+//         setUsers(res.data);
+//         console.log(res.data);
+//       })
+//       // .then((response) => {
+//       //   // checking if there are users in the database
+//       //   if (users && response.data.length > 0) {
+//       //     console.log(response);
+//       //     // if (userName && response.data.length > 0)
+//       //     // const user = location.state.username;
+//       //     // console.log(user);
+//       //     // console.log(response);
+//       //     // setUsers({
+//       //     //   // returning each user in the array by username
+//       //     //   users: response.data.map((user) => user.username),
+//       //     //   // setting the first element in the array to be the first user displayed
+//       //     //   username: user,
+//       //     // });
+//       //     setUsers(users);
+//       //     // setUserName(userName);
+//       //     // console.log(userName);
+//       //   }
+//       //   console.log(users);
+//       // })
+//       .catch((error) => console.log(error));
+//   }, []);
+
+//   function onchangeUsername(e) {
+//     setUserName(e.target.value);
+//   }
+//   function onchangeDescription(e) {
+//     setDescription(e.target.value);
+//   }
+//   function onchangeDuration(e) {
+//     setDuration(e.target.value);
+//   }
+//   function onchangeDate(e) {
+//     setDate(e.target.value);
+//   }
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     const workout = {
+//       username: userName,
+//       description: description,
+//       duration: duration,
+//       date: date,
+//     };
+
+//     const name = {
+//       username: userName,
+//     };
+
+//     axios
+//       .post("/workouts/add", workout)
+//       .then((res) => {
+//         history.push({
+//           pathname: "/workoutlist",
+//           state: { username: name },
+//         });
+//         console.log(res.data);
+//       })
+//       .catch((err) => console.log(err));
+//   }
+
+//   // Framer motion animations
+//   const cardVariants = {
+//     initial: {
+//       opacity: 0,
+//       x: "-500vw",
+//       scale: 0,
+//     },
+//     in: {
+//       opacity: 1,
+//       x: 0,
+//       scale: 1,
+//     },
+//     out: {
+//       opacity: 0,
+//       x: "500vw",
+//       scale: 0,
+//     },
+//   };
+//   // Framer motion animations
+//   const cardTransition = {
+//     type: "tween",
+//     ease: "anticipate",
+//     duration: 0.8,
+//   };
+
+//   return (
+//     <div className="create-container">
+//       <motion.div
+//         className="create-container__card"
+//         initial="initial"
+//         animate="in"
+//         exit="out"
+//         variants={cardVariants}
+//         transition={cardTransition}
+//       >
+//         <div className="create-container__title-container">
+//           <h2 className="create-container__title">CREATE WORKOUT</h2>
+//         </div>
+//         <div className="create-container__form-container">
+//           <form
+//             onSubmit={handleSubmit}
+//             className="create-container__form"
+//             action=""
+//           >
+//             <div className="create-container__username-container">
+//               <label className="create-container__username-label" htmlFor="">
+//                 USERNAME
+//               </label>
+//               <select
+//                 className="create-container__username"
+//                 // value={userName}
+//                 value={users}
+//                 multiple={true}
+//                 onChange={onchangeUsername}
+//               >
+//                 {/* console.log(users) */}
+//                 {/* users.map((users)=>(
+//                 <option key={users} value={users}>
+//                   {users}
+//                 </option> */}
+//                 ) )
+//                 {/* {Object.entries(users).users.map((user) => {
+//                   console.log(user);
+//                   return (
+//                     <option key={user} value={user}>
+//                       {user}
+//                     </option>
+//                   );
+//                 })} */}
+//                 {/* <input
+//                 className="create-container__username"
+//                 type="text"
+//                 value={userName}
+//                 onChange={onchangeUsername}
+//               /> */}
+//                 {
+//                   users.map((user) => {
+//                     return <option value={user}>{user}</option>;
+//                   })
+//                   // console.log(users)
+//                 }
+//               </select>
+//             </div>
+//             <div className="create-container__description-container">
+//               <label className="create-container__description-label" htmlFor="">
+//                 DESCRIPTION
+//               </label>
+//               <textarea
+//                 className="create-container__description"
+//                 value={description}
+//                 onChange={onchangeDescription}
+//               />
+//             </div>
+//             <div className="create-container__duration-container">
+//               <label className="create-container__duration-label" htmlFor="">
+//                 DURATION (in Minutes)
+//               </label>
+//               <input
+//                 className="create-container__duration"
+//                 type="number"
+//                 min="0"
+//                 max="59"
+//                 value={duration}
+//                 onChange={onchangeDuration}
+//               />
+//             </div>
+//             <div className="create-container__date-container">
+//               <label className="create-container__date-label" htmlFor="">
+//                 DATE
+//               </label>
+//               <input
+//                 className="create-container__date"
+//                 type="date"
+//                 selected={date}
+//                 onChange={onchangeDate}
+//               />
+//             </div>
+//             <div className="create-container__button-container">
+//               <button className="create-container__button">LET'S GO !</button>
+//             </div>
+//           </form>
+//         </div>
+//       </motion.div>
+//     </div>
+//   );
+// };
+
+// export default CreateWorkout;
