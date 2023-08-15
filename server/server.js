168aac217d8cd55b8c962616e018d2fd5c93d70e
Abdul <kukluu@hotmail.com>
2023-08-15 13:56:27 -0700

    deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ['https://fi-track-frontend.vercel.app/'],
+    origin: ['https://fi-track-frontend.vercel.app'],
   })
 );
 app.use(express.json()); // Parsing JSON
